b6013623224a85680c1593a878b0e707bdffbca6
Alshival <samuel@alshival.com>
2024-01-20 06:37:15 -0600

    need to clean up code, but it is functional. Adding live shard updates.

--- bot.py
+++ bot.py
@@ -21,7 +21,10 @@ sky_bot_token = os.environ.get("sky_bot_token")
 # Set up bot with '!' command prefix.
 bot = commands.Bot(command_prefix="!",intents=discord.Intents.all())
 
-@bot.tree.command(name="hug")
+@bot.tree.command(
+    name="hug",
+    description="Virtual hugs"
+)
 @app_commands.checks.has_permissions(use_application_commands=True)
 async def hug(interaction: discord.Interaction, member: discord.Member):
     await interaction.response.defer(thinking = True)
@@ -33,41 +36,55 @@ async def hug(interaction: discord.Interaction, member: discord.Member):
         icon_url=interaction.user.avatar)
     await interaction.followup.send(member.mention,embed = embed1,file=discord.File('public/emojis/icon_hug.webp', filename="map_image.webp"))
 
-from src import shardPredictor
 
-@bot.tree.command(name='shard_of_the_day')
-async def shard_of_the_day(interaction: discord.Interaction):
-    await interaction.response.defer(thinking = True)
-    
+#####################################
+# Shards
+#####################################
+from src import shardPredictor
+async def get_shard_info_response(date=datetime.utcnow()):
     # Get today's date as string
-    today_date_str = datetime.utcnow()
+    today_date_str = date
     # Get shard info for today
     shard_info = shardPredictor.get_shard_info(today_date_str)
     
     # Image path
     map_image_path = f"public/infographics/map_clement/{shard_info['map']}.webp"
     occurrences = ",".join([f"<t:{int(x.timestamp())}:t>" for x in shard_info['occurrences']])
-    # You can customize the response based on the shard info
+    
+    # Customize the response based on the shard info
     if shard_info['haveShard']:
         if shard_info['isRed']:
-            response = "<:ShardRed:1198069899973636137> **Red Shard Today:**\n"\
+            response = f"{config['ShardRed_emoji']} **Red Shard Today:**\n"\
                        f"Realm: {shard_info['realm']}\n"\
                        f"Map: {shard_info['map']}\n"\
-                       f"Reward AC: {shard_info['rewardAC']} <:AscendedCandle:1198069985017331852>\n"\
-                       f"Occurences: {occurrences}"
+                       f"Reward AC: {shard_info['rewardAC']} {config['AscendedCandle_emoji']}\n"\
+                       f"Occurrences: {occurrences}"
         else:
-            response = "<:ShardBlack:1198069944697503894>  **Black Shard Today:**\n"\
+            response = f"{config['ShardBlack_emoji']} **Black Shard Today:**\n"\
                        f"Realm: {shard_info['realm']}\n"\
                        f"Map: {shard_info['map']}\n"\
-                       f"Occurences: {occurrences}"
-        await interaction.followup.send(response,file=discord.File(map_image_path, filename="map_image.webp"))
-
+                       f"Occurrences: {occurrences}"
+        return response, map_image_path
     else:
-        response = f"**No Shard today!**"
+        return f"**No Shard today!**", 'public/noShard.gif'
 
-        await interaction.followup.send(response,file=discord.File('public/noShard.gif', filename="map_image.webp"))
+@bot.tree.command(
+    name='shard_of_the_day',
+    description="Get information about Today's shard"
+)
+async def shard_of_the_day(interaction: discord.Interaction):
+    await interaction.response.defer(thinking=True)
 
-@bot.tree.command(name='next_shards')
+    # Get shard info and response
+    response, map_image_path = await get_shard_info_response()
+    
+    # Post the shard of the day message
+    await interaction.followup.send(response, file=discord.File(map_image_path, filename="map_image.webp"))
+
+@bot.tree.command(
+    name='next_shards',
+    description="Retrieve the next few shards"
+)
 @app_commands.choices(
     only =[
         app_commands.Choice(name="Red Shards",value="red"),
@@ -98,14 +115,139 @@ async def next_shards(interaction: discord.Interaction,n: int = 5,  only: app_co
 
     await interaction.followup.send(response)
 
+
+@tasks.loop(minutes=3)  # Update every 3 minutes (adjust as needed)
+async def post_live_shard_updates():
+    async with aioduckdb.connect(db_name) as connection:
+        async with connection.cursor() as cursor:
+            # Check if the table exists
+            await cursor.execute(
+                """
+                SELECT EXISTS (
+                    SELECT 1 FROM information_schema.tables 
+                    WHERE table_name = 'live_shard_channel'
+                ) as table_exists
+                """
+            )
+            table_exists_result = await cursor.fetchone()
+            table_exists = table_exists_result[0]
+
+            if table_exists:
+                # Fetch data from the table
+                await cursor.execute(
+                    "SELECT guild_id, channel_id, message_id FROM live_shard_channel"
+                )
+                results = await cursor.fetchall()
+                
+                for result in results:
+                    guild_id, channel_id, message_id = result
+                    guild = bot.get_guild(guild_id)
+                    channel = guild.get_channel(channel_id) if guild else None
+                    if channel:
+                        response, map_image_path = await get_shard_info_response()
+                        # Try to get the existing message
+                        existing_message = None
+                        try:
+                            existing_message = await channel.fetch_message(message_id)
+                        except discord.NotFound:
+                            pass
+
+                        if existing_message:
+                            await existing_message.edit(content=response, attachments=[discord.File(map_image_path, filename="map_image.webp")])
+                        else:
+                            new_message = await channel.send(response, file=discord.File(map_image_path, filename="map_image.webp"))
+                            # Update the message_id in the database
+                            await cursor.execute(
+                                "UPDATE live_shard_channel SET message_id = ? WHERE guild_id = ? AND channel_id = ?",
+                                (new_message.id, guild_id, channel_id),
+                            )
+                    else:
+                        response = f"No Shard today!"
+                        # Try to get the existing message
+                        existing_message = None
+                        try:
+                            existing_message = await channel.fetch_message(message_id)
+                        except discord.NotFound:
+                            pass
+
+                        if existing_message:
+                            await existing_message.edit(content=response, attachments=[discord.File('public/noShard.gif', filename="map_image.webp")])
+                        else:
+                            new_message = await channel.send(response, file=discord.File('public/noShard.gif', filename="map_image.webp"))
+                            # Update the message_id in the database
+                            await cursor.execute(
+                                "UPDATE live_shard_channel SET message_id = ? WHERE guild_id = ? AND channel_id = ?",
+                                (new_message.id, guild_id, channel_id),
+                            )
+            else:
+                print("live_shard_channel table does not exist yet. Skipping update.")
+
+#####################################
 # Administrative stuff
+#####################################
+@bot.tree.command(
+    name="set_live_shard_channel",
+    description="Set up the channel for live shard updates."
+)
+@app_commands.checks.has_permissions(administrator=True)
+async def set_live_shard_channel(interaction: discord.Interaction, channel: discord.TextChannel):
+    await interaction.response.defer(thinking=True)
+    async with aioduckdb.connect(db_name) as connection:
+        await connection.execute(
+            """
+            CREATE TABLE IF NOT EXISTS live_shard_channel (
+                guild_id BIGINT,
+                channel_id BIGINT,
+                message_id BIGINT
+            )
+            """
+        )
+        # Post the initial message and store the message_id in the database
+
+        response, map_image_path = await get_shard_info_response()
+        message = await interaction.followup.send("**Live Updates:** \n" + response,file=discord.File(map_image_path, filename="map_image.webp"))
+        async with aioduckdb.connect(db_name) as connection:
+            await connection.execute(
+                "INSERT INTO live_shard_channel (guild_id, channel_id,message_id) VALUES (?, ?,?)",
+                (interaction.guild_id, channel.id,message.id),
+            )
+
+@bot.tree.command(
+    name="remove_live_shard_channel",
+    description="Remove all live shard updates from the server."
+)
+@app_commands.checks.has_permissions(administrator=True)
+async def remove_live_shard_channel(interaction: discord.Interaction):
+    await interaction.response.defer(thinking=True)
+    
+    async with aioduckdb.connect(db_name) as connection:
+        # Check if the guild has a live shard channel set up
+        result = await connection.execute(
+            "SELECT * FROM live_shard_channel WHERE guild_id = ?",
+            (interaction.guild_id,),
+        )
+        row = await result.fetchone()
+        
+        if row:
+            # If there is a record, remove the channel from the database
+            await connection.execute(
+                "DELETE FROM live_shard_channel WHERE guild_id = ?",
+                (interaction.guild_id,),
+            )
+            
+            await interaction.followup.send("Live shard channel removed successfully.")
+        else:
+            await interaction.followup.send("No live shard channel set up for this guild.")
+
+
 @bot.tree.command(
     name="set_daily_quest_channel",
-    description="Set up the channel to be cleared daily."
+    description="Set up a channel to be cleared before daily resets"
 )
 @app_commands.checks.has_permissions(administrator=True)
 async def set_daily_quest_channel(interaction: discord.Interaction, channel: discord.TextChannel):
     await interaction.response.defer(thinking=True)
+    shard_info = getattr(bot, 'shard_info', None)
     async with aioduckdb.connect(db_name) as connection:
         await connection.execute(
             """
@@ -129,7 +271,7 @@ async def clear_daily_quest_channel(bot):
     clear_daily_quest_channel_running = True
     
     now = datetime.now(pendulum.timezone('America/Los_Angeles'))  # Set to Pacific Time Zone
-    if now.hour == 23 and now.minute == 55 :
+    if now.hour == 23 and now.minute == 58:
         try:
             # Iterate over all guilds
             for guild in bot.guilds:
@@ -150,9 +292,9 @@ async def clear_daily_quest_channel(bot):
                             )
                             channel_result = await cursor.fetchone()
 
-                        elif not (table_exists and channel_result):
-                            print(f"Daily quest channel not set up for guild {your_guild_id} or table does not exist. Skipping.")
-                            continue
+                        elif not table_exists:
+                            print(f"Daily quest channel table does not exist. Skipping.")
+                            return
 
                 # Retrieve channel ID from the database based on the guild ID
                 async with aioduckdb.connect(db_name) as connection:
@@ -177,65 +319,16 @@ async def clear_daily_quest_channel(bot):
         finally:
             clear_daily_quest_channel_running = False
 
-# clear_daily_quest_channel_running = False
-# @tasks.loop(minutes=1)
-# async def clear_daily_quest_channel(bot):
-#     your_guild_id = bot.guilds[0].id
-#     global clear_daily_quest_channel_running
-    
-#     # Check if the table exists and a channel has been set
-#     async with aioduckdb.connect(db_name) as connection:
-#         async with connection.cursor() as cursor:
-#             await cursor.execute(
-#                 "SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'daily_quest_channel') as table_exists"
-#             )
-#             table_exists_result = await cursor.fetchone()
-#             table_exists = table_exists_result[0]
-#             if table_exists:
-#                 await cursor.execute(
-#                     "SELECT channel_id FROM daily_quest_channel"
-#                 )
-#                 channel_result = await cursor.fetchone()
-    
-#             elif not (table_exists and channel_result):
-#                 print("Daily quest channel not set up or table does not exist. Skipping.")
-#                 return
-    
-#     clear_daily_quest_channel_running = True
-    
-#     now = datetime.now(pendulum.timezone('America/Los_Angeles'))  # Set to Pacific Time Zone
-#     if now.hour == 23 and now.minute == 55 :
-#         try:
-#             # Retrieve channel ID from the database based on the guild ID
-#             async with aioduckdb.connect(db_name) as connection:
-#                 async with connection.cursor() as cursor:
-#                     await cursor.execute(
-#                         "SELECT distinct guild_id,channel_id FROM daily_quest_channel WHERE guild_id = ?",
-#                         (your_guild_id,),
-#                     )
-#                     result = await cursor.fetchone()
-
-#                     if result:
-#                         channel_id = result[1]
-#                         channel = bot.get_channel(channel_id)
-
-#                         if channel:
-#                             await channel.purge(limit=1000)
-#                             print("Daily quest channel cleared.")
-#                     else:
-#                         print("Daily quest channel not set up.")
-#         except Exception as e:
-#             print(f"An error occurred: {e}")
-#         finally:
-#             clear_daily_quest_channel_running = False
-
-
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
     if not clear_daily_quest_channel_running:
         clear_daily_quest_channel.start(bot)
+        
+    #update_shard_info.start()
+    post_live_shard_updates.start()
+    
     try:
         synced = await bot.tree.sync()
         print(f"Synced {len(synced)} slash command(s)")
